c567f2583f2a9a6866c9839488339a8dbde7ea7d
Pablo Pazos <pablopazosg@gmail.com>
2012-01-19 15:30:34 +0000

    largest number

--- src/main/java/com/develogical/WebServer.java
+++ src/main/java/com/develogical/WebServer.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-// http://192.168.40.30:3000/players/881280c0
+// http://192.168.40.30:3000/players/d2936180
 
 public class WebServer extends HttpServlet {
 
@@ -38,17 +38,32 @@ public class WebServer extends HttpServlet {
         if (query == null) {
             return result;
         }
-        
-        String[] strings = query.split(" ");
+
+//        String[] strings = query.split(" ");
+//        System.out.println("Length: " + strings.length);
+//
+//        if (strings.length == 4 && strings[3].equals(("name"))) {
+//            System.out.println(" strings[3]: " + strings[3]);
+//            result = "severe_summer";
+//        }
+//
+//        System.out.println("Returning: " + result);
+//        return result;
+
+        String[] strings = query.split(":");
         System.out.println("Length: " + strings.length);
 
-        if (strings.length == 4 && strings[3].equals(("name"))) {
-            System.out.println(" strings[3]: " + strings[3]);
-            result = "severe_summer";
-        }
+        String[] numbers = strings[2].replaceAll(" ", "").split(",");
         
-        System.out.println("Returning: " + result);
-        return result;
+        return getMax(numbers).toString();
+        
+//        if (strings.length == 4 && strings[3].equals(("name"))) {
+//            System.out.println(" strings[3]: " + strings[3]);
+//            result = "severe_summer";
+//        }
+//
+//        System.out.println("Returning: " + result);
+//        return result;
     }
 
     public static void main(String[] args) throws Exception {
@@ -68,5 +83,24 @@ public class WebServer extends HttpServlet {
         server.start();
         server.join();
     }
+    
+    public String getMax(String[] list){
+        Integer max = 0;
+        
+        if(list.length < 1) {
+            max = -1;
+        }
+        else{
+            max= Integer.parseInt(list[0]);
+            for(int i=0; i<list.length; i++) {
+                if(max < Integer.parseInt(list[i])) {
+                    max = Integer.parseInt(list[i]);
+                }
+            }
+        }
+        
+        return max.toString();
+    }
+
 }
 
--- src/test/java/com/develogical/WebServerTest.java
+++ src/test/java/com/develogical/WebServerTest.java
@@ -20,4 +20,12 @@ public class WebServerTest {
     public void testProcessMyName() throws Exception {
         assertEquals("severe_summer", server.process("what is your name"));
     }
+
+    /**
+     * Ensure that a 'bad' request gets "Unknown" as a response.
+     */
+    @org.junit.Test
+    public void testProcessLargestNumber() throws Exception {
+        assertEquals("503", server.process("74e9afc0: which of the following numbers is the largest: 60, 503"));
+    }
 }
